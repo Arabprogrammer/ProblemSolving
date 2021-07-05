e1b5db010d2cd2279c019f6badbb8129305493ac
Arabprogrammer <Arabprogrammer3030@gmail.com>
2021-07-05 15:22:49 +0200

    ChatOrder

--- src/com/app/ChatOrder.java
+++ src/com/app/ChatOrder.java
@@ -1,5 +1,9 @@
 package com.app;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ChatOrder {
 
 	/*
@@ -7,7 +11,14 @@ public class ChatOrder {
 	 * 
 	 */
 	public static void main(String[] args) {
-		Integer numberOfPolycarpus = 4;
+		List<String> names = Arrays.asList("alex", "ivan", "roman", "ivan");
+		List<String> sortedNames = new ArrayList<>();
+		for (int i = names.size() - 1; i >= 0; i--) {
+			String name = names.get(i);
+			if (!sortedNames.contains(name))
+				sortedNames.add(name);
+		}
+		System.err.println(sortedNames);
 
 	}
 
